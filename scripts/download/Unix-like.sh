a14551d5fde7b47f1bbf82901b6cf17a517dd996
mfz <>
2025-01-03 23:13:50 +0800

    perf: Compatible without gh

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -3,7 +3,7 @@ set -eu
 echo ::group::Downloading Rclone $version for $RUNNER_OS $RUNNER_ARCH
 if [ $RUNNER_OS = macOS ]; then os=osx; else os=linux; fi
 if [ $RUNNER_ARCH = ARM64 ]; then arch=arm64; else arch=amd64; fi
-if [ -n "$FORGEJO_ACTIONS" ] || [ -n "$GITEA_ACTIONS" ]; then
+if [ -z "${FORGEJO_ACTIONS+x}" ] || [ -z "${GITEA_ACTIONS+x}" ]; then
 	# in forgejo or gitea runner
     wget -q https://github.com/rclone/rclone/releases/download/$version/rclone-$version-$os-$arch.zip
 else
