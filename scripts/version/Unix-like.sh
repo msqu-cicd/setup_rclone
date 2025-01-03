e473fbffc395e668721491158bbe1a8a31df7ade
mfz <>
2025-01-03 22:38:41 +0800

    perf: Compatible without gh

--- scripts/version/Unix-like.sh
+++ scripts/version/Unix-like.sh
@@ -2,8 +2,15 @@
 set -eu
 if [ "$version" = latest ]
 then
-  latest_version=$(gh api repos/rclone/rclone/releases/latest -q .tag_name)
-  echo "version=$latest_version" >> $GITHUB_OUTPUT
+  if ! command -v gh &>/dev/null; then
+    # gh not found
+    latest_version=$(curl -s https://api.github.com/repos/rclone/rclone/releases/latest | jq -r .tag_name)
+    echo "version=$latest_version" >> $GITHUB_OUTPUT
+  else
+    # gh found
+    latest_version=$(gh api repos/rclone/rclone/releases/latest -q .tag_name)
+    echo "version=$latest_version" >> $GITHUB_OUTPUT
+  fi
 else
   echo "version=$version" >> $GITHUB_OUTPUT
 fi
--- scripts/version/Windows.ps1
+++ scripts/version/Windows.ps1
@@ -1,9 +1,27 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
-if ($env:version -eq 'latest') {
-  $latest_version = (gh api repos/rclone/rclone/releases/latest -q .tag_name)
-  Add-Content $env:GITHUB_OUTPUT version=$latest_version
+
+# GitHub API URL for latest release
+$githubApiUrl = "https://api.github.com/repos/rclone/rclone/releases/latest"
+
+# Check if 'gh' command is available
+if (Get-Command gh -ErrorAction SilentlyContinue) {
+  # If 'gh' is available, use it to fetch the latest version
+  if ($env:version -eq 'latest') {
+    $latest_version = (gh api repos/rclone/rclone/releases/latest -q .tag_name)
+    Add-Content $env:GITHUB_OUTPUT "version=$latest_version"
+  } else {
+    Add-Content $env:GITHUB_OUTPUT "version=$env:version"
+  }
+} else {
+  # If 'gh' is not available, fall back to using REST API to fetch the latest version
+  if ($env:version -eq 'latest') {
+    $response = Invoke-RestMethod -Uri $githubApiUrl -Method Get -Headers @{Accept = 'application/vnd.github.v3+json' }
+    $latest_version = $response.tag_name
+    Add-Content $env:GITHUB_OUTPUT "version=$latest_version"
+  } else {
+    Add-Content $env:GITHUB_OUTPUT "version=$env:version"
+  }
 }
-else {
-  Add-Content $env:GITHUB_OUTPUT version=$env:version
-}
+
+# Write-Output $latest_version
