2c62236088a36f97976705487ad7c4214fbc5e44
mfz <>
2025-01-03 23:19:49 +0800

    perf: Compatible without gh

--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -2,7 +2,11 @@ $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 Set-StrictMode -Version Latest
 Write-Host ::group::Downloading Rclone $env:version for Windows
-Invoke-WebRequest $env:GITHUB_SERVER_URL/rclone/rclone/releases/download/$env:version/rclone-$env:version-windows-amd64.zip -OutFile rclone-$env:version-windows-amd64.zip
+if ($null -ne $env:GITEA_ACTIONS -and $env:GITEA_ACTIONS -ne "") {
+    Invoke-WebRequest https://github.com/rclone/rclone/releases/download/$env:version/rclone-$env:version-windows-amd64.zip -OutFile rclone-$env:version-windows-amd64.zip
+} else {
+    Invoke-WebRequest $env:GITHUB_SERVER_URL/rclone/rclone/releases/download/$env:version/rclone-$env:version-windows-amd64.zip -OutFile rclone-$env:version-windows-amd64.zip
+}
 Expand-Archive rclone-$env:version-windows-amd64.zip Rclone
 Move-Item Rclone\rclone-$env:version-windows-amd64\* Rclone
 Remove-Item rclone-$env:version-windows-amd64.zip, Rclone\rclone-$env:version-windows-amd64
